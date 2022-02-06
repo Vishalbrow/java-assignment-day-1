ee0a4bcdf53d8b338e61e8230f6a6c555fc7db9a
Vishalbrow <99115642+Vishalbrow@users.noreply.github.com>
2022-02-06 13:29:30 +0530

    Day 1 committed

---
+++ day1.py
@@ -0,0 +1,131 @@
+package com.codewithharry;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+	 // Write your code here
+//        System.out.println("Hello World");
+     /* Variables
+     Just Like:
+        -Water - Bucket
+        -Masala - box
+        -Lunch - LunchBox
+     In Java:
+     Variables are containers which store data values
+     String, int, float, char, boolean
+     How to declare variables:
+     syntax - <dataType> <variableName> = <value>;
+      */
+     String name = "Harry";
+     String channel = "CodeWithHarry";
+//     System.out.println(name);
+//     System.out.println(name.length());
+//     System.out.println(name.toUpperCase());
+//     System.out.println(name.toLowerCase());
+//     System.out.println(name + " from\" " + channel);
+//     System.out.println(name + " from\\ " + channel);
+//     System.out.println(name + " from\t " + channel);
+//     System.out.println(name + " from\n " + channel);
+
+//        System.out.println(name.contains("Har"));
+//        System.out.println(name.charAt(2));
+//        System.out.println(name.endsWith("ry"));
+//        System.out.println(name.indexOf("rry"));
+
+        int numb1 = 4, numb2 = 7;
+//        System.out.println(Math.max(numb1, numb2));
+//        System.out.println(Math.min(numb1, numb2));
+//        System.out.println(Math.sqrt(36));
+//        System.out.println(Math.abs(-36));
+//        System.out.println(Math.abs(6));
+//        System.out.println(Math.random());
+//        System.out.println(4+(8-4)*Math.random());
+//        System.out.println(4+(8-4)*Math.random());
+//        System.out.println(4+(8-4)*Math.random());
+//        System.out.println(4+(8-4)*Math.random());
+//        System.out.println(4+(8-4)*Math.random());
+
+
+     int a = 45, x=56, y=67;
+     float b = 45.22f;
+     boolean isAdult = false;
+//     System.out.println(x);
+//     System.out.println(b);
+//     System.out.println(isAdult);
+
+    /* Rules for constructing name of variables in Java
+        1. Can contain digits, underscores, dollar signs, letters
+        2. Should begin with a letter, $ or _
+        3. Java is case sensitive language which means that
+           harry and Harry are two different variables altogether.
+        4. Should not contain whitespaces
+        5. You cannot use reserved keywords from Java
+     */
+    /*
+    Two types of Java Data Types:
+        1. Primitive - byte (1 byte), short (2 bytes), int(4 bytes), long (8 bytes), float(4 bytes),
+           double (8 bytes), boolean(1 bit), char (2 bytes).
+        2. Non Primitive or Reference Data Type -
+    */
+    byte u = -56;
+    double d = 45.635435345d;
+//    System.out.println(d);
+    char grade = 'A';
+//    System.out.println(grade);
+
+    /* Operators in Java
+
+        Operand, operator, Operand  =  Result
+        4           +           7   =  11
+
+        Types of operators in Java
+        Arithmetic operators
+        Assignment operators
+        Logical Operators
+        Comparison Operators
+     */
+    int num1 = 45, num2=78;
+    num1 += 3;
+    num2 -= 8;
+    // Explore these operators - *=, /=, %=
+//        System.out.print("The value of num1 + num2 is ");
+//        System.out.println(num1 + num2);
+//
+//        System.out.print("The value of num1 - num2 is ");
+//        System.out.println(num1 - num2);
+//
+//        System.out.print("The value of num1 * num2 is ");
+//        System.out.println(num1 * num2);
+//
+//        System.out.print("The value of num1 / num2 is ");
+//        System.out.println(num1 / num2);
+//
+//        System.out.print("The value of num1 % num2 is ");
+//        System.out.println(num1 % num2);
+//        System.out.println(num2++);
+//        System.out.println(++num1);
+//        System.out.println(num1--);
+//        System.out.println(--num1);
+
+    /*
+    * Comparison Operators:
+    * 1. == : checks for equality of two values
+    * 2. != : checks if two values are not equal
+    * 3. <
+    * 4. >
+    * 5. <=
+    * 6. >=
+    *
+    * Logical Operators:
+    * 1. && - Logical and operator - returns true only if both conditions are true
+    * 2. || - Logical or operator - returns true if any one condition is true
+    * 3. ! - Logical not - Reverse the result from true to false and vice versa
+    * */
+
+ 
